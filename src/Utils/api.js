7538a1095be4599fd92d059dbd56d5b7f492fe33
Carl Janz <carl.janz@gmail.com>
2020-01-27 23:00:10 -0800

    expanded mock game data

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,6 +1,30 @@
 const TEMP_GAME_DATA = {
+  round: 3,
+  publicObjectives: [
+    { roundRevealed: 1, id: "Sway the Council", score: 1 },
+    { roundRevealed: 1, id: "Develop Weaponry", score: 1 },
+    { roundRevealed: 2, id: "Erect a Monument", score: 1 },
+    { roundRevealed: 3, id: "Corner the Market", score: 1 }
+  ],
   players: [
-    { name: "Carl", faction: "Nekrovirus", points: [{ score: 1 }] },
+    {
+      name: "Carl",
+      faction: "Nekrovirus",
+      points: [
+        {
+          type: "Secret Objective",
+          id: "Destroy Their Greatest Ship",
+          roundClaimed: 2,
+          score: 1
+        },
+        {
+          type: "Public Objective",
+          id: "Sway the Council ",
+          roundClaimed: 3,
+          score: 1
+        }
+      ]
+    },
     { name: "Sean", faction: "Sardaak Nor", points: [] }
   ],
   gameId: "12345",
