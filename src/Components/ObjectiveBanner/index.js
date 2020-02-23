3e256ecdbaedb6cca4ad88df3b2bdc2f2d13b4dc
Carl Janz <carl.janz@gmail.com>
2020-02-23 15:20:26 -0800

    Added objective icons to objective banners

--- src/Components/ObjectiveBanner/index.js
+++ src/Components/ObjectiveBanner/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import ObjectiveCard from "../ObjectiveCard";
 import CardListModal from "../CardListModal";
+import helpers from "../../Utils/helpers";
+import Icon from "../Icon";
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  width: 250px;
-  border: black solid 1px;
   display: inline-block;
+`;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 40px 235px;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: "icon title" "icon content";
+  width: 275px;
+  border: black solid 1px;
+  // display: inline-block;
   margin: 5px;
   border-radius: 5px;
 `;
@@ -17,12 +24,19 @@ const TitleContainer = styled.div`
   background: black;
   color: white;
   padding-bottom: 5px;
+  grid-area: title;
 `;
+
+const IconContainer = styled.div`
+  grid-area: icon;
+`;
+
 const Title = styled.span`
-  font-size: 1.5rem;
+  font-size: 1.2rem;
 `;
-const Content = styled.div`
+const ContentContainer = styled.div`
   padding-top: 3px;
+  grid-area: content;
   min-height: 29px;
   & > * {
     margin: 0 2px;
@@ -33,13 +47,18 @@ export default function ObjectiveBanner(props) {
   const { name, type, pointvalue } = props;
   return (
     <Wrapper>
-      <TitleContainer>
-        <Title>{name}</Title>
-        <CardListModal count={1} vertical>
-          <ObjectiveCard {...props} />
-        </CardListModal>
-      </TitleContainer>
-      <Content>{props.children}</Content>
+      <GridContainer>
+        <IconContainer>
+          <Icon size="40" {...helpers.getObjectiveTypeById(type)} />
+        </IconContainer>
+        <TitleContainer>
+          <Title>{name}</Title>
+          <CardListModal count={1} vertical>
+            <ObjectiveCard {...props} />
+          </CardListModal>
+        </TitleContainer>
+        <ContentContainer>{props.children}</ContentContainer>
+      </GridContainer>
     </Wrapper>
   );
 }
